83129ae1fcb749c4c464612565799475dd0b87c2
noahcarnegie0478 <noah.carnegie0478@outlook.com>
2025-06-07 00:38:06 +1000

    updated code

--- src/day18/Main.java
+++ src/day18/Main.java
@@ -3,16 +3,16 @@ package day18;
 public class Main {
 	public static void main(String[] args) {
 	int[] moneyPaper = {500,200,100,50,20,10,5,2,1};
-	int cashNumer = findMoneys(moneyPaper, 512);
+	int cashNumer = findMoneys(moneyPaper, 467);
 	System.out.println(cashNumer);
 	}
 	public static int findMoneys(int[] moneyPaper, int value) {
 		int cashNumber = 0;	
 		for (int i = 0; i < moneyPaper.length; i++) {		
 			if (value / moneyPaper[i] > 0) {
-				int posiblilityCash =  value /moneyPaper[i];
-				value = value -  moneyPaper[i]*posiblilityCash;			
-				cashNumber = cashNumber + posiblilityCash;
+				cashNumber = cashNumber + (value / moneyPaper[i]);
+				value = value % moneyPaper[i];		
+				
 			}		
 		}	
 		return cashNumber;
